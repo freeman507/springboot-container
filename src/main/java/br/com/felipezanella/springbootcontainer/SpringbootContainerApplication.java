002b10f7007ccd6513657770f604db2842a2ebb9
Felipe Zanella <freeman.zanella@gmail.com>
2020-06-16 15:41:20 -0300

    first commit

---
+++ src/main/java/br/com/felipezanella/springbootcontainer/SpringbootContainerApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipezanella.springbootcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipezanella/springbootcontainer/SpringbootContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipezanella.springbootcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
